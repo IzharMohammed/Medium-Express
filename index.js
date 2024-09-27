c5c3031734ae05399b9f779acfd248d444b1836d
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-09-27 22:53:54 +0530

    Added rooms

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const http = require('http');
 
 // Create an Express application
 const app = express();
-
+app.use(express.json());
 // Create an HTTP server with Express
 const server = http.createServer(app);
 
@@ -19,6 +19,29 @@ app.use(cors());
 app.get('/test', (req, res) => {
     res.json({ msg: "successful" });
 });
+
+
+let globalRoomId = '';   // Global variable for room ID
+let allUserIds = [];
+
+function createGlobalVariables(roomId, userIds) {
+    globalRoomId = roomId;
+    allUserIds = userIds;
+}
+
+app.post('/api/create-room', (req, res) => {
+    const { roomId, userIds } = req.body;
+    console.log('roomid', roomId);
+    console.log('userIds', userIds);
+    createGlobalVariables(roomId, userIds);
+
+    //console.log(`senderId: ${senderId}, receiverId: ${receiverId}`);
+    //console.log(response);
+    // userIds.forEach(userId => {
+    //     socketIo.to(userId).emit('join_room', req.body);
+    // })
+    res.json({ msg: "successul in entering" })
+})
 
 // Initialize Socket.IO with the HTTP server and configure CORS
 const socketIo = require("socket.io")(server, {
@@ -35,10 +58,24 @@ socketIo.on("connection", (socket) => {
     // Listen for 'message' events from clients
     socket.on("message", (data) => {
         // Log the received message data
-        console.log(data);
+        console.log('message', data);
         // Broadcast the message data to all connected clients
-        socketIo.emit("messageResponse", data);
+        socketIo.to('room_10_13').emit("messageResponse", data);
     });
+
+    socket.on('join_room', (roomId) => {
+
+        const [_, senderId, receiverId] = roomId.split('_');
+        // console.log(`senderId: ${senderId},receiverId: ${receiverId} `);
+        console.log(` id ${globalRoomId}`);
+
+        console.log(`user 1 :${allUserIds[0]}, user 2 : ${allUserIds[1]}`);
+
+        if (parseInt(senderId) == allUserIds[0] && parseInt(receiverId) == allUserIds[1]) {
+            socket.join(roomId);
+            console.log(`${roomId} created`);
+        }
+    })
 
     // Handle client disconnection
     socket.on("disconnect", () => {
@@ -46,6 +83,7 @@ socketIo.on("connection", (socket) => {
         console.log('disconnected');
     });
 });
+
 
 // Start the HTTP server and listen on the defined port
 server.listen(PORT, () => {
